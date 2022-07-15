5054c8718efe58498f885a7d6f307aa7b969efc3
justinozzy <jetoswald@gmail.com>
2022-07-15 00:20:47 -0700

    Migration to TypeScript

--- client/main.js
+++
@@ -1,5 +0,0 @@
-import keys from '../constants/keys.json' assert {type: 'json'};
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import key from "./constants/keys.json";
+import { Client } from "discord.js";
+
+console.log(key.DiscordAPIKey);
