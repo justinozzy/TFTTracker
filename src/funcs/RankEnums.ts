3ffb76b47642f31e7005f4dee9bcb3ffe91dfd5b
justinozzy <jetoswald@gmail.com>
2022-07-18 19:02:09 -0700

    Added enums based on rank

---
+++ src/funcs/RankEnums.ts
@@ -0,0 +1,18 @@
+export enum rankTier {
+    'IV' = 0,
+    'III' = 1,
+    'II' = 2,
+    'I' = 3
+}
+
+export enum rankLadder {
+    IRON = 0,
+    BRONZE = 1,
+    SILVER = 2,
+    GOLD = 3,
+    PLATINUM = 4,
+    DIAMOND = 5,
+    MASTER = 6,
+    GRANDMASTER = 7,
+    CHALLENGER = 8
+}
